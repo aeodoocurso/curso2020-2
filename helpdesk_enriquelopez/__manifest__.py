82b34101cf6ffd756ddcc6745dc66c00d1105b5f
Enrique López de Roda López <e.lrl@hotmail.com>
2020-12-20 16:44:37 +0100

    W5_tasks.md dia 2 ok

--- helpdesk_enriquelopez/__manifest__.py
+++ helpdesk_enriquelopez/__manifest__.py
@@ -22,14 +22,15 @@ Módulo de prueba de Helpdesk curso Aeodoo
         "mail"
     ],
     "data": [
+    'data/helpdesk_data.xml',
     'security/helpdesk_security.xml',
     'security/ir.model.access.csv',
     'wizards/new_ticket_from_tag_views.xml',
     'views/helpdesk_ticket_views.xml',
     'views/helpdesk_tag_views.xml',
     'views/helpdesk_action_views.xml',
-    'data/helpdesk_data.xml',
     ],
     "demo": [
+    'data/helpdesk_demo.xml',
     ],
 }
